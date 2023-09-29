a9b11f37373ec3c48df3953855c05ef4c1112c52
zahralatif <zahralatif2107@gmail.com>
2023-09-29 19:47:39 +0400

    equality condition

--- NumberComparison.java
+++ NumberComparison.java
@@ -11,11 +11,15 @@ public class NumberComparison {
 
     if (sumAB < sumCD) {
       System.out.println(
-        "The sum of 20 and 10 is less than the sum of 30 and 40."
+        "The sum of " + A + " and " + B + " is less than the sum of " + C + " and " + D
+      );
+    } else if (sumAB > sumCD) {
+      System.out.println(
+        "The sum of " + A + " and " + B + " is not less than the sum of " + C + " and " + D
       );
     } else {
       System.out.println(
-        "The sum of 20 and 10 is not less than the sum of 30 and 40."
+        "The sum of " + A + " and " + B + " is equal the sum of " + C + " and " + D
       );
     }
   }
